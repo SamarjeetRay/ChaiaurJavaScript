0775b3b3073d1c59abf6ca9ec016960208367139
Samarjeet Ray <samarjeetkumarray01@gmail.com>
2024-08-18 21:44:03 +0530

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,21 @@
-const accountId = 970850    //const can't be modified
-let accountEmail = "samarjeeet@gmail.com"  //let can be modified 
-var accountPassword = "12345"  //var can be modified
-accountCity = "Jaipur" //this can't be modified
-let accountState;   //it will be undefined
+const accountId = 970850; //const can't be modified
+let accountEmail = "samarjeeet@gmail.com"; //let can be modified
+var accountPassword = "12345"; //var can be modified
+accountCity = "Jaipur"; //this can't be modified
+let accountState; //it will be undefined
 // accountId = 12345
-accountEmail = "utsav@gmail.com"
-accountPassword = "00000"
-accountCity="kolkata"
+accountEmail = "utsav@gmail.com";
+accountPassword = "00000";
+accountCity = "kolkata";
 console.log(accountId);
 
-console.table([ accountId, accountEmail,accountPassword, accountCity, accountState]);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
 
 /*
 prefer not to use var because of issue in block scope and functional scope
