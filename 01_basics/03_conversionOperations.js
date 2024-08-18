6aa53060c4d710904884cb779fde02aa7b7de216
Samarjeet Ray <samarjeetkumarray01@gmail.com>
2024-08-19 00:13:42 +0530

    Conversions od dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+// let score = "samar" ;
+
+// console.log(typeof score);
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// basic conversion
+//"33" => 33
+//"33abc" => NaN (not a number)
+// true => 1 , flase => 0
+
+// let isLoggedIn = "samar"
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "samar" => true
+
+let someNumber = 45;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
