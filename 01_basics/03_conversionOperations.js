dbebb4e299fdd8b0cf95b9da1e81b8f5a0a3d7cc
Samarjeet Ray <samarjeetkumarray01@gmail.com>
2024-08-19 00:17:18 +0530

    conversions of DataTypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,8 @@
-// let score = "samar" ;
+ let score = "samar" ;
 
 // console.log(typeof score);
 // console.log(typeof score);
-// let valueInNumber = Number(score);
+ let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
@@ -11,17 +11,18 @@
 //"33abc" => NaN (not a number)
 // true => 1 , flase => 0
 
-// let isLoggedIn = "samar"
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
+let isLoggedIn = "samar"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
-
+ 
+//basic conversions
 // 1 => true , 0 => false
 // "" => false
 // "samar" => true
 
 let someNumber = 45;
 let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
 
 
