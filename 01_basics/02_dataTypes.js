af945c07241f91f9d8cb283aa75d3c0e812220b5
Samarjeet Ray <samarjeetkumarray01@gmail.com>
2024-08-18 21:43:42 +0530

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code a newer version
+
+// alert(3+3) ??we are using node.js , not browser v8
+
+console.log(3 + 3); //code readability should be high
+
+console.log("Samar");
+
+let name = "Samar";
+let age = 22;
+let isLoggedIn = false;
+let state = null;
+
+// number =>    2 to power 53
+// bigint =>    for bigger numeric value
+// string =>    ""
+// boolean =>   true/false
+// null    =>   standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);    // output will be undefined
+
+console.log(typeof null);        //output will be object
